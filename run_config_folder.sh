9eedfd3e8b43b27dfb4798ea947d3347d966c8b0
Lukas Tuggener <tugg@zhaw.ch>
2022-06-01 18:43:30 +0200

    fix path

--- run_config_folder.sh
+++ run_config_folder.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# ./run_config_folder.sh rn18_configs/* /home/ubuntu/ImageNet_ffcv 12 /home/ubuntu/rotation_module/ffcv-imagenet/logs
+# ./run_config_folder.sh 'rn18_configs/*' /home/ubuntu/ImageNet_ffcv 12 /home/ubuntu/rotation_module/ffcv-imagenet/logs
 #configs_path="rn18_configs/*"
 for FILE in ${1}; do
   python train_imagenet.py \
