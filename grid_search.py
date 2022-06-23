a499d2cce521d272829eec24d0cb94349d7f5903
Lukas Tuggener <tugg@zhaw.ch>
2022-06-23 17:20:53 +0200

    fix grid search bug

--- grid_search.py
+++ grid_search.py
@@ -37,7 +37,7 @@ configs_dict["--logging.wandb_project"] = "training_angle_classifiers"
 configs_dict["--training.load_from"] = ["mask_rotate"]
 configs_dict["--angleclassifier.freeze_base"] = [0, 1]
 configs_dict["--angleclassifier.classifier"] = ['fc', 'fc2']
-configs_dict["--angleclassifier.classifier"] = [0, 1, 2, 3]
+configs_dict["--angleclassifier.angle_regress"] = [0, 1, 2, 3]
 
 configs_dict["--data.in_memory"] = 1
 #configs_dict["--training.epochs"] = 1
