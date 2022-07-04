0e9083cafbca22c1c1b889719fd29bece2c47a90
Lukas Tuggener <tugg@zhaw.ch>
2022-07-04 18:31:00 +0200

    keep deep class channels the same

--- grid_search.py
+++ grid_search.py
@@ -37,7 +37,7 @@ configs_dict["--logging.wandb_project"] = "regress2_training_angle_classifiers"
 configs_dict["--training.load_from"] = ["mask_rotate"]
 configs_dict["--angleclassifier.freeze_base"] = [1]
 configs_dict["--lr.lr"] = [0.1]
-configs_dict["--angleclassifier.classifier"] = ['deep']
+configs_dict["--angleclassifier.classifier"] = ['deep', 'deepx2', 'deepslant']
 configs_dict["--angleclassifier.angle_regress"] = [2]
 configs_dict["--angleclassifier.flatten"] = ['basic']
 
@@ -46,7 +46,7 @@ configs_dict["--angleclassifier.flatten"] = ['basic']
 
 
 configs_dict["--data.in_memory"] = 1
-#configs_dict["--training.epochs"] = 1
+configs_dict["--training.epochs"] = 1
 
 def extend_commands(commands:list, append:str) -> list:
     for i, command in enumerate(commands):
