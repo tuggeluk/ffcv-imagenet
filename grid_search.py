1875e8901f634e9a5bb78650a4592555a049eefe
Lukas Tuggener <tugg@zhaw.ch>
2022-07-22 18:10:28 +0200

    grid search on all base models

--- grid_search.py
+++ grid_search.py
@@ -60,7 +60,7 @@ configs_dict["--angle_testmode.corr_pred"] = [1]
 
 
 configs_dict["--data.in_memory"] = 1
-configs_dict["--training.epochs"] = 1
+configs_dict["--training.epochs"] = 33
 
 def extend_commands(commands:list, append:str) -> list:
     for i, command in enumerate(commands):
