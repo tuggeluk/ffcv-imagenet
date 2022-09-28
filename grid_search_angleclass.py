622c7391d689958b0fc7e960fd8bd42ef489b52d
Lukas Tuggener <tugg@zhaw.ch>
2022-09-28 12:09:38 +0200

    add stanford cars

--- grid_search_angleclass.py
+++ grid_search_angleclass.py
@@ -65,8 +65,8 @@ configs_dict["--validation.double_rotate"] = 1
 configs_dict["--dist.port"] = 12253
 
 
-configs_dict["--resolution.max_res"] = [500]
-configs_dict["--resolution.min_res"] = 500
+configs_dict["--resolution.max_res"] = [450]
+configs_dict["--resolution.min_res"] = 450
 
 configs_dict["--training.double_resize"] = 1
 configs_dict["--validation.double_resize"] = 1
