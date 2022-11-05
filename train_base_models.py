1a84b2a885c03bcb5f60214cdf777bc8a8f5476e
Lukas Tuggener <tugg@zhaw.ch>
2022-11-05 11:40:48 +0100

    mask more pixels

--- train_base_models.py
+++ train_base_models.py
@@ -24,14 +24,13 @@ else:
 
 configs_dict["--data.num_workers"] = 12
 configs_dict["--data.in_memory"] = 1
-configs_dict["--data.dataset"] = 'ImageNet'
+configs_dict["--data.dataset"] = 'StanfordCars'
 configs_dict["--logging.wandb_dryrun"] = 0
-configs_dict["--logging.wandb_project"] = "train_base_models_VIT"
-#configs_dict["--model.arch"] = ['resnet18', 'resnet50', 'resnet152', 'efficientnet_b0', 'efficientnet_b2', 'efficientnet_b4',
-#                                'resnext50_32x4d', 'resnext101_32x8d']
+configs_dict["--logging.wandb_project"] = "train_base_models_stanfordcars"
+configs_dict["--model.arch"] = ['resnet18', 'resnet50', 'resnet152', 'efficientnet_b0', 'efficientnet_b2', 'efficientnet_b4',
+                                'resnext50_32x4d', 'resnext101_32x8d']
 configs_dict["--training.random_rotate"] = [0, 1]
-configs_dict["--model.pretrained"] = [0]
-configs_dict["--model.arch"] = ['vit_b_32']
+#configs_dict["--model.arch"] = ['vit_b_32', 'vit_l_32']
 
 #configs_dict["--training.epochs"] = 1
 
